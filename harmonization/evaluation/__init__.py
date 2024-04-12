d9fcc3d2569cbb2be3e132c56ea7423769794010
KogniJannis <104796901+KogniJannis@users.noreply.github.com>
2024-04-12 17:31:22 +0200

    add imports in init

--- harmonization/evaluation/__init__.py
+++ harmonization/evaluation/__init__.py
@@ -4,3 +4,5 @@ Module related to the Human Alignment benchmark
 
 from .clickme import load_clickme_val, evaluate_clickme
 from .metrics import dice, spearman_correlation, intersection_over_union
+from .main import evaluate_model
+from .explainers import torch_saliency_explainer, tensorflow_explainer
